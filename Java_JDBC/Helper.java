31ebd7d946c84cdfa50a9058aac941b7a24872ea
Mrinal-K-Mishra <107672054+Mrinal-K-Mishra@users.noreply.github.com>
2022-09-13 14:40:02 +0530

    Update Helper.java

--- Java_JDBC/Helper.java
+++ Java_JDBC/Helper.java
@@ -15,6 +15,6 @@ public class Helper {
 		}
 
 	public static Connection con() throws SQLException {
-		return DriverManager.getConnection("jdbc:mysql://localhost:3506/mrinaldb","root","root");
+		return DriverManager.getConnection("jdbc:mysql://localhost:3306/mrinaldb","root","root");
 	}
 }
